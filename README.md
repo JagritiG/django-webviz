2d853abe94d247238b918494bf6e883054dbd10e
JagritiG <jagritigoswami84@gmail.com>
2021-03-30 23:43:12 -0700

    Add new files and modify readme

--- README.md
+++ README.md
@@ -49,7 +49,7 @@ This is a simple Django app for web visualization which allows users to upload c
 - Install requirements.txt  using pip - **$ pip install –r requirements.txt**
 
         
-### Deployment
+### Deployment on Localhost:8000
 - Navigate to project webviz  (project name)
 - Run command ls to check the right path: 
 [__init__.py  db.sqlite3  manage.py  media  static  viz  webviz]
@@ -60,5 +60,66 @@ This is a simple Django app for web visualization which allows users to upload c
 - Go to **Upload File**, **localhost:8000/upload/** and upload a csv file and plot the figure
 - To navigate to **admin** open **localhost:8000/admin/** 
 (admin name: admin and password: webviz123)
+
+### Deployment on Live server using cPanel
+1. Log into cPanel Account 
+2. Open File Manager
+3. Zip project-folder and upload in the File manager/root directory (e.g. webviz.zip)
+4. Database Setup: (MySQL)
+    - Navigate to cPanel 
+    - Open **DATABASES -> MySQL Database**
+    - Create database for the project: give <**database-name**> and click on <**Create Database**> (e.g. webviz)
+    - click <**Go Back**>
+    - Create user (Give user name, password) and click on <**Create User**> (e.g. user1, user123)
+    - click <**Go Back**>
+    - Add user to the database: in the section <**Add User To Database**> -> 
+    select user and database and click on <**Add**>
+    - Give Privileges to this user: select all the privileges and click on <**Make Changes**>
+    - Now navigate to **cPanel -> MySQL Database** -> you can see database is created.
+
+5. Configure database credential in **settings.py** -> 
+    ###
+        DATABASES = {
+                'default': {
+                    'ENGINE': 'django.db.backends.mysql', 
+                    'NAME': 'database-name',
+                    'USER': 'user-name',
+                    'PASSWORD': 'password',
+                    'HOST': 'localhost', 
+                    'PORT': '3306',
+                }
+        }
+    
+6. Configure domain name in **settings.py**
+    ###
+        ALLOWED_HOSTS = ['domain_name.com', 'www.domain_name.com']
+
+7. Configure Python App 
+    - Navigate to **cPanel -> SOFTWARE** and Click on <**Python Setup App**>
+    - Click <**Create Application**>
+    - Select python version - (e.g. 3.8.6)
+    - Add Application root -> <**project-name**> (e.g. webviz)
+    - Set Application url -> select **domain_name.com**
+    - Click <**Create App**> 
+    
+8. Install requirements.txt file
+    - Navigate to **cPanel -> SOFTWARE** and Click on <**Python Setup App**>
+    - Select Aap -> <**Edit**>
+    - Add **requirements.txt** file and click on <**Run Pip Install**> to install all the required modules
+
+9. Create Database Table
+    - Navigate to **cPanel -> SOFTWARE** and Click on <**Python Setup App**>
+    - Select Aap -> <Edit>
+    - Run Script: **manage.py migrate**
+    - To check Database table navigate to ****cPanel -> DATABASES -> phpMyAdmin**
+
+10. Configure **passenger_wsgi.py**
+    - add **from webviz.wsgi import application**
+    ###
+          import os
+          import sys
+          
+          from webviz.wsgi import application
+
 
 
--- webviz/viz/views.py
+++ webviz/viz/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 import pandas as pd
 from plotly.offline import plot
 import plotly.graph_objects as go
 import time
-
+from django.http import HttpResponseNotFound
 
 from .forms import UploadCsvForm
 from .models import Csv
@@ -60,87 +61,88 @@ def data_process(request):
 
 def data_visualize(request):
     context = {}
-    if request.method == 'POST':
-        plots = request.POST.get('Plot', None)
-        context['plots'] = plots
+    plots = request.POST.get('Plot', None)
+    context['plots'] = plots
 
-        get_objects = Csv.objects.latest('title')
-        uploaded_file = get_objects.csv
-        context['get_objects'] = get_objects
+    get_objects = Csv.objects.latest('title')
+    uploaded_file = get_objects.csv
+    context['get_objects'] = get_objects
 
-        df = pd.read_csv(uploaded_file, encoding='utf-8')
-        col_list = df.columns.to_list()
-        context['columns'] = col_list
-    else:
-        return render(request, 'viz/data_visualize.html')
+    df = pd.read_csv(uploaded_file, encoding='utf-8')
+    col_list = df.columns.to_list()
+    context['columns'] = col_list
 
     return render(request, 'viz/data_visualize.html', context)
 
 
 def chart(request):
+
     context = {}
-    chart_type = request.POST.get('chart-type')
-    x_var = request.POST.get('x-var')
-    y_var = request.POST.get('y-var')
+    if request.method == 'POST':
+        chart_type = request.POST.get('chart-type')
+        x_var = request.POST.get('x-var')
+        y_var = request.POST.get('y-var')
 
-    # retrieve dataframe
-    get_objects = Csv.objects.latest('title')
-    uploaded_file = get_objects.csv
-    df = pd.read_csv(uploaded_file, encoding='utf-8')
+        # retrieve dataframe
+        get_objects = Csv.objects.latest('title')
+        uploaded_file = get_objects.csv
+        df = pd.read_csv(uploaded_file, encoding='utf-8')
 
-    # Plot bar chart
-    if chart_type == "Bar":
-        x = df[x_var]
-        y = df[y_var]
-        fig = go.Figure()
-        bar_chart = go.Bar(x=x, y=y)
-        fig.add_trace(bar_chart)
-
-        fig.update_layout(title_text=get_objects.title)
-        fig.update_xaxes(title_text=x_var)
-        fig.update_yaxes(title_text=y_var)
-
-        plot_div = plot(fig, output_type='div')
-        context['plot_div'] = plot_div
-
-    # Scatter Plot
-    if chart_type == "Scatter":
-        x = df[x_var]
-        y = df[y_var]
-        fig = go.Figure()
-        scatter_plot = go.Scatter(x=x, y=y, mode='markers')
-        fig.add_trace(scatter_plot)
-
-        fig.update_layout(title_text=get_objects.title)
-        fig.update_xaxes(title_text=x_var)
-        fig.update_yaxes(title_text=y_var)
-
-        plot_div = plot(fig, output_type='div')
-        context['plot_div'] = plot_div
-
-    # Scatter Plot
-    if chart_type == "Histogram":
-        if x_var and y_var is None:
+        # Plot bar chart
+        if chart_type == "Bar":
             x = df[x_var]
+            y = df[y_var]
             fig = go.Figure()
-            hist_plot = go.Histogram(x=x, histnorm='probability')
-            fig.add_trace(hist_plot)
+            bar_chart = go.Bar(x=x, y=y)
+            fig.add_trace(bar_chart)
 
             fig.update_layout(title_text=get_objects.title)
             fig.update_xaxes(title_text=x_var)
+            fig.update_yaxes(title_text=y_var)
+
             plot_div = plot(fig, output_type='div')
             context['plot_div'] = plot_div
 
-        if y_var and x_var is None:
+        # Scatter Plot
+        if chart_type == "Scatter":
+            x = df[x_var]
             y = df[y_var]
             fig = go.Figure()
-            hist_plot = go.Histogram(y=y, histnorm='probability')
-            fig.add_trace(hist_plot)
+            scatter_plot = go.Scatter(x=x, y=y, mode='markers')
+            fig.add_trace(scatter_plot)
 
             fig.update_layout(title_text=get_objects.title)
+            fig.update_xaxes(title_text=x_var)
             fig.update_yaxes(title_text=y_var)
+
             plot_div = plot(fig, output_type='div')
             context['plot_div'] = plot_div
+
+        # Scatter Plot
+        if chart_type == "Histogram":
+            if x_var and y_var is None:
+                x = df[x_var]
+                fig = go.Figure()
+                hist_plot = go.Histogram(x=x, histnorm='probability')
+                fig.add_trace(hist_plot)
+
+                fig.update_layout(title_text=get_objects.title)
+                fig.update_xaxes(title_text=x_var)
+                plot_div = plot(fig, output_type='div')
+                context['plot_div'] = plot_div
+
+            if y_var and x_var is None:
+                y = df[y_var]
+                fig = go.Figure()
+                hist_plot = go.Histogram(y=y, histnorm='probability')
+                fig.add_trace(hist_plot)
+
+                fig.update_layout(title_text=get_objects.title)
+                fig.update_yaxes(title_text=y_var)
+                plot_div = plot(fig, output_type='div')
+                context['plot_div'] = plot_div
+    else:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
 
     return render(request, 'viz/chart.html', context)
 
@@ -148,5 +150,7 @@ def chart(request):
 def dashboard(request):
     context = {}
     if request.method == 'POST':
-        print("Dashboard")
+        print("dashboard")
     return render(request, 'viz/dashboard.html', context)
+
+
